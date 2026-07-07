b301d104d9abded2836fb102627e8f7cefef9953
Sebastian Renk <sebastian.renk16@gmail.com>
2026-07-07 22:24:04 +0200

    implemented player movement

--- main.cpp
+++ main.cpp
@@ -8,6 +8,31 @@ int main() {
     Map map;
     Player player(2, 3);
     map.draw(player);
+    bool running{true};
+    while (running) {
+        char input;
+        std::cin >> input;
+        switch(input) {
+            case 'w':
+                player.move(0, -1);
+                break;
+            case 'a':
+                player.move(-1, 0);
+                break;
+            case 's':
+                player.move(0, 1);
+                break;
+            case 'd':
+                player.move(1, 0);
+                break;
+        }
+        if (input == 'e')
+            running = false;
+        for (int i{0}; i < 50; ++i)
+            std::cout << '\n';
+        map.draw(player);
+
+    }
     
     return 0;
 }
--- player.h
+++ player.h
@@ -13,5 +13,4 @@ class Player {
         void move(int x, int y);
 };
 
-
 #endif
